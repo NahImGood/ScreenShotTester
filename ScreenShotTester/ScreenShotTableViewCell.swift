16fca4cb7865fa37258cccb3481c12b9566ca2f5
NahImGood <warnere225@gmail.com>
2019-01-29 18:06:37 -0800

    Initial Commit

---
+++ ScreenShotTester/ScreenShotTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  ScreenShotTableViewCell.swift
+//  ScreenShotTester
+//
+//  Created by Admin on 1/29/19.
+//  Copyright © 2019 EGW. All rights reserved.
+//
+
+import UIKit
+
+class ScreenShotTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var cityName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // MARK: Test Data
+    
+    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+                "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+                "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+                "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+                "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
+
+
+}
